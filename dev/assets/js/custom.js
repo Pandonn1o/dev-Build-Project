c379daf68305291c7f75f89305aaa75616dbe1d4
misha dyshkant <mishadyshkant12@gmail.com>
2024-03-11 19:19:22 +0200

    dekstop 1-st version, burger btb

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,28 @@
-  
+
+const  InstallGenplan = ()=>{
+    const adress = document.querySelector('#adress');
+    const floors = document.querySelector('#floor');
+    const flats = document.querySelector('#flat');
+    const builds = document.querySelectorAll('.build-path');
+    //
+    builds.forEach(build => {
+        build.addEventListener('mouseover', ()=>{
+            const buildAdress = build.getAttribute('data-address');
+            const buildFloor = build.getAttribute('data-floor-amount');
+            const buildFlats = build.getAttribute('data-flat-quaintity');
+            adress.innerHTML = buildAdress;
+            floors.innerHTML = buildFloor;
+            flats.innerHTML = buildFlats;
+        })
+    })
+}
+document.querySelector('.genplan') ? InstallGenplan() : null;
+const headerNav = document.querySelector('.header-nav');
+const burger = document.querySelector('.burger-btn');
+
+burger.addEventListener('click', ()=>{
+    headerNav.classList.toggle('show');
+})
+burger.addEventListener('click', ()=>{
+    burger.classList.toggle('is-active');
+})
