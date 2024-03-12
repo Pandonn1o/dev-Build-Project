6d012667b3e56b0157bf60f33df2bd48c6d30f28
misha dyshkant <mishadyshkant12@gmail.com>
2024-03-12 18:05:34 +0200

    first-page: done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,22 +1,3 @@
-
-const  InstallGenplan = ()=>{
-    const adress = document.querySelector('#adress');
-    const floors = document.querySelector('#floor');
-    const flats = document.querySelector('#flat');
-    const builds = document.querySelectorAll('.build-path');
-    //
-    builds.forEach(build => {
-        build.addEventListener('mouseover', ()=>{
-            const buildAdress = build.getAttribute('data-address');
-            const buildFloor = build.getAttribute('data-floor-amount');
-            const buildFlats = build.getAttribute('data-flat-quaintity');
-            adress.innerHTML = buildAdress;
-            floors.innerHTML = buildFloor;
-            flats.innerHTML = buildFlats;
-        })
-    })
-}
-document.querySelector('.genplan') ? InstallGenplan() : null;
 const headerNav = document.querySelector('.header-nav');
 const burger = document.querySelector('.burger-btn');
 
@@ -26,3 +7,36 @@ burger.addEventListener('click', ()=>{
 burger.addEventListener('click', ()=>{
     burger.classList.toggle('is-active');
 })
+const  InstallGenplan = ()=>{
+    const adress = document.querySelector('#adress');
+    const floors = document.querySelector('#floor');
+    const flats = document.querySelector('#flat');
+    const build = document.querySelectorAll('.build-path');
+    //
+    build.forEach(build => {
+        build.addEventListener('mouseover', ()=>{
+            const buildAdress = build.getAttribute('data-address');
+            const buildFloor = build.getAttribute('data-floor-amount');
+            const buildFlats = build.getAttribute('data-flat-quaintity');
+            adress.innerHTML = buildAdress;
+            floors.innerHTML = buildFloor;
+            flats.innerHTML = buildFlats;
+        })
+    })
+    const booking = (build)=> build.forEach(build =>{
+        const buildLink = build.closest('a');
+        const flatAmount = build.getAttribute('data-flat-quaintity');
+        //const flatAmountToNumber = Number(flatAmount);
+        // const flatAmountToNumber = flatAmount * 1;
+        // const flatAmountToNumber = +build.getAttribute('data-flat-quaintity');
+        const flatAmountToNumber = parseInt(flatAmount);
+        flatAmountToNumber ? flatAmountToNumber >= 0 : buildLink.classList.add('booking');
+        
+        buildLink.addEventListener('click', ()=> {
+            if(buildLink.classList.contains('booking'))
+            event.preventDefault();
+        })
+    })
+    booking(build);
+}
+document.querySelector('.genplan') ? InstallGenplan() : null;
